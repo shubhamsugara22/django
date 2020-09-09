64b3beaa98e06d24eb1521e58e319e949dd7b7ec
shubhamsugara22 <shubhamsugara22@gmail.com>
2020-09-09 14:16:28 +0530

    B3

---
+++ newyear/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+# Create your views here.
+def index(request):
+    now=datetime.datetime.now()
+    return render(request,"newyear/index.html",{
+        "newyear":now.month==1 and now.date==1            #Change value to TRUE to check if it works
+    })
