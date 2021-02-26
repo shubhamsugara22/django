8573f1c394e83404bfee8faef1d6f997afd1ad76
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-02-26 23:30:33 +0530

    testing start

--- flights/models.py
+++ flights/models.py
@@ -2,23 +2,34 @@ from django.db import models
 
 # Create your models here.
 
+
 class Airport(models.Model):
     code = models.CharField(max_length=3)
     city = models.CharField(max_length=64)
 
     def __str__(self):
-        return f"{self.city} {self.code}"   
+        return f"{self.city} {self.code}"
+
+
 class Flight(models.Model):
-    origin = models.ForeignKey( Airport ,on_delete=models.CASCADE , related_name = "departures")
-    destination = models.ForeignKey( Airport ,on_delete=models.CASCADE ,related_name="arrivals")
-    duration = models.IntegerField()    
+    origin = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="arrivals")
+    duration = models.IntegerField()
+
     def __str__(self):
         return f"{self.origin} to {self .destination}"
 
+    def is_valid_flight(self):
+        return self.origin! = self.destination or self.duration >= 0
+
+
 class Passenger(models.Model):
     first = models.CharField(max_length=64)
-    last  = models.CharField(max_length=64)
-    flights = models.ManyToManyField(Flight ,blank=True,related_name="passenger")
+    last = models.CharField(max_length=64)
+    flights = models.ManyToManyField(
+        Flight, blank=True, related_name="passenger")
+
     def __str__(self):
         return f"{self.first} {self.last}"
-    
