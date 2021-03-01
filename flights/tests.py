48bbf5747ba7ceea6f989fa2a052e11ca915f9fd
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-03-01 13:40:55 +0530

    adding test cases

--- flights/tests.py
+++ flights/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from .models import Airport, Flight, Passenger
 # Create your tests here.
+
+
+class FlightTestCase(TestCase):
+
+    def setUp(self):
+        a1 = Airport.objects.create(code="AAA", city="City A")
+        a2 = Airport.objects.create(code="BBB", city="City B")
+
+        Flight.objects.create(origin=a1, destination=a2, duration=120)
+        Flight.objects.create(origin=a1, destination=a1, duration=140)
+        Flight.objects.create(origin=a1, destination=a2, duration=180)
+
+    def test_departures_count(self){
+        a = Airport.objects.get(code="AAA")
+        self.assertEqual(a.departures.count(), 3)
+    }
+
+    def test_arrivals_count(self){
+        a = Airport.objects.get(code="AAA")
+        self.assertEqual(a.arrivals.count(), 3)
+    }
