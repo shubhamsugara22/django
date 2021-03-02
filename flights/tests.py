3269b06cb53f9279d10c2a67dd09df7b7c08bf3a
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-03-02 23:09:02 +0530

    added more test cases

--- flights/tests.py
+++ flights/tests.py
@@ -13,12 +13,21 @@ class FlightTestCase(TestCase):
         Flight.objects.create(origin=a1, destination=a1, duration=140)
         Flight.objects.create(origin=a1, destination=a2, duration=180)
 
-    def test_departures_count(self){
+    def test_departures_count(self):
         a = Airport.objects.get(code="AAA")
         self.assertEqual(a.departures.count(), 3)
-    }
 
-    def test_arrivals_count(self){
+    def test_arrivals_count(self):
         a = Airport.objects.get(code="AAA")
         self.assertEqual(a.arrivals.count(), 3)
-    }
+
+    def test_valid_flight(self):
+        a1 = Airport.objects.create(code="AAA", city="City A")
+        a2 = Airport.objects.create(code="BBB", city="City B")
+        f = Flight.objects.get(origin=a1,  destination=a2, duration=100)
+        self.assertTrue(f.is_valid_flight())
+
+    def test_invalid_flight_destination(self):
+        a1 = Airport.objects.create(code="AAA")
+        f = Flight.objects.get(origin=a1,  destination=a1)
+        self.assertFalse(f.is_valid_flight())
